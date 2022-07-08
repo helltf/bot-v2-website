fa851ed33abe5dfdc22783afa221ad07fdfdb6db
helltf <Dario.Zachow5530@gmail.com>
2022-07-08 17:57:14 +0200

    chore: moving links in seperate component

---
+++ src/lib/components/NavLink.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { Link } from 'svelte-routing'
+	export let linkName
+	export let linkTo
+</script>
+
+<li class="hover:text-green-800 transition-all">
+	<Link to={linkTo}>{linkName}</Link>
+</li>
