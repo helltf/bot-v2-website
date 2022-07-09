936abb33cac9171063a46f00e6841b7307a57347
helltf <Dario.Zachow5530@gmail.com>
2022-07-09 15:50:47 +0200

    feat: creating cards for given projects

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -1,3 +1,21 @@
-<section class="">
-	<h1>Info</h1>
+<script>
+	import ProjectCard from './ProjectCard.svelte'
+
+	const projects = [
+		['helltfbot-v2', 'Version 2 of helltfbot', ''],
+		['bot-v1-fullstack', 'Fullstack v1', ''],
+		['twitchbot', 'Old Twitchbot', ''],
+		['bot-v2-backend', 'Backend for v2', ''],
+		['bot-v2-website', 'Frontend for v2', ''],
+		['go-discord-bot', 'Old discord bot', ''],
+	]
+</script>
+
+<section class="pr-20 pl-20">
+	<h2 class="mb-10 text-3xl">Projects</h2>
+	<div class=" grid grid-cols-2 gap-4 justify-items-center">
+		{#each projects as [repoName, title, icon]}
+			<ProjectCard {icon} {title} {repoName} />
+		{/each}
+	</div>
 </section>
