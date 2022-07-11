4053a9e5176dced652a04d3a58666b901fa788c0
helltf <Dario.Zachow5530@gmail.com>
2022-07-11 19:33:35 +0200

    feat: Botcard to display all bots

---
+++ src/lib/components/BotCard.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let bg
+	export let description
+	export let title
+	export let href
+
+	let hover = 'hover:'
+</script>
+
+<div
+	class="w-60 flex flex-col {bg} p-6 pr-9 pl-9 rounded-lg gap-5 lg:w-[400px] w-[600px]"
+>
+	<h1 class="text-3xl ">{title}</h1>
+	<p class="text-base  lg:w-[300px] w-[500px]">{description}</p>
+	<a
+		class="w-[70%] hover:bg-neutral-300 transition-all bg-white rounded-sm flex flew-col justify-between p-2"
+		{href}
+	>
+		<span class="text-neutral-500 text-sm">view commands</span>
+		<img src="../../arrow.svg" alt="arrow" />
+	</a>
+</div>
