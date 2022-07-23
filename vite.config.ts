97f261c8a6d169714b30551882a38599d9d243aa
helltf <Dario.Zachow5530@gmail.com>
2022-07-23 09:50:08 +0200

    chore: use basepath only in prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import 'dotenv/config'
+
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
-	base: '/bot-v2-website/',
+	base: process.env.VITE_ENV === 'prod' ? '/bot-v2-website/' : '.',
 	server: { port: 3000 },
 })
