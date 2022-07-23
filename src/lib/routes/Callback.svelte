4a7742684ba5aa065e1178dc5254652d87db627e
helltf <Dario.Zachow5530@gmail.com>
2022-07-23 09:50:46 +0200

    formatting: creating prettierrc

--- src/lib/routes/Callback.svelte
+++ src/lib/routes/Callback.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
-	import { navigate } from 'svelte-routing'
+  import { onMount } from 'svelte'
+  import { navigate } from 'svelte-routing'
 
-	const getToken = (): string | null =>
-		new URLSearchParams(document.location.search).get('code')
+  const getToken = (): string | null => new URLSearchParams(document.location.search).get('code')
 
-	onMount(() => {
-		const token = getToken()
+  onMount(() => {
+    const token = getToken()
 
-		navigate('home')
-	})
+    navigate('home')
+  })
 </script>
