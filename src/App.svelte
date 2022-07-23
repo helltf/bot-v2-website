bf19d848258c112ce16424d6c64b0dff10ba5aff
helltf <Dario.Zachow5530@gmail.com>
2022-07-23 09:50:08 +0200

    chore: use basepath only in prod

--- src/App.svelte
+++ src/App.svelte
@@ -9,31 +9,34 @@
 </script>
 
 <main>
-  <TailwindCss />
+	<TailwindCss />
 
-  <Router {url} basepath="/bot-v2-website/">
-    <Nav />
-    <div>
-      <Route path=""><Home /></Route>
-      <Route path="info"><h1>Oh</h1></Route>
-      <Route path="commands"><h1>Oh</h1></Route>
-      <Route path="stats"><h1>Oh</h1></Route>
-      <Route path="callback"><Callback /></Route>
-    </div>
-  </Router>
+	<Router
+		{url}
+		basepath={import.meta.env.VITE_ENV === 'prod' ? '/bot-v2-website/' : '.'}
+	>
+		<Nav />
+		<div>
+			<Route path=""><Home /></Route>
+			<Route path="info"><h1>Oh</h1></Route>
+			<Route path="commands"><h1>Oh</h1></Route>
+			<Route path="stats"><h1>Oh</h1></Route>
+			<Route path="callback"><Callback /></Route>
+		</div>
+	</Router>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    background-color: #282828;
-    color: #ffffff;
-    height: 100%;
-  }
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+			Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		background-color: #282828;
+		color: #ffffff;
+		height: 100%;
+	}
 
-  *::after,
-  *::before {
-    box-sizing: border-box;
-  }
+	*::after,
+	*::before {
+		box-sizing: border-box;
+	}
 </style>
