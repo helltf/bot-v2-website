e18d605c18b3d34a8d89ad536a3e3cda4b3a4c52
DarioZachow <dario.zachow@deutschebahn.com>
2022-07-26 18:03:04 +0200

    fix: correcting default base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
-	base: process.env.VITE_ENV === 'prod' ? '/bot-v2-website/' : '.',
+	base: process.env.VITE_ENV === 'prod' ? '/bot-v2-website/' : './',
 	server: { port: 3000 },
 })
