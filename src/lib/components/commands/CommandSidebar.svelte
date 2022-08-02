53fc7321427efc942fb154b5799fcd7b47eb36c6
helltf <Dario.Zachow5530@gmail.com>
2022-08-02 20:21:07 +0200

    feat: creating sidebar

---
+++ src/lib/components/commands/CommandSidebar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let commands: string[]
+</script>
+
+<div class="flex flex-col overflow-y-scroll ">
+  <section class="text-neutral-300 text-xl pb-2">Commands</section>
+
+  {#each commands as command}
+    <a href="">{command}</a>
+  {/each}
+</div>
