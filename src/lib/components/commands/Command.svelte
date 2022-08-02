df2a5d70e031c4e8cc595dd8289b1a039aca8c18
helltf <Dario.Zachow5530@gmail.com>
2022-08-02 20:20:40 +0200

    chore: moving into seperate package

---
+++ src/lib/components/commands/Command.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { fetchCommands } from '../../../lib/js/commands'
+  import type { Command } from '../../../lib/js/commands'
+
+  import { onMount } from 'svelte'
+  import CommandSidebar from './CommandSidebar.svelte'
+
+  let commands: Command[] = []
+
+  onMount(async () => {
+    commands = await fetchCommands(import.meta.env.VITE_BACKEND_URL + 'commands')
+  })
+
+  const getNames = (): string[] => {
+    return commands.map((command) => command.name)
+  }
+</script>
+
+<div class="flex flex-row ml-40 mr-40 gap-5">
+  <div class="w-[200px]">
+    {#if commands.length}
+      <CommandSidebar commands={getNames()} />
+    {/if}
+  </div>
+  <div class="w-[100%] h-[100%] bg-white" />
+</div>
