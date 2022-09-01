3ebbfccda2fdb8410bb6d46cb871f70c397997d9
helltf <Dario.Zachow5530@gmail.com>
2022-09-01 08:58:07 +0200

    feat(routes): creating fallback routes for commands and login

--- src/App.svelte
+++ src/App.svelte
@@ -4,18 +4,23 @@
 	import { Router, Route } from 'svelte-routing'
 	import Home from './lib/routes/Home.svelte'
 	import Callback from './lib/routes/Callback.svelte'
+	import Command from './lib/routes/Command.svelte'
+	import CommandForward from './lib/routes/CommandForward.svelte'
 	export let url = ''
 </script>
 
 <main class="h-full">
-  <TailwindCss />
+	<TailwindCss />
 
-	<Router {url} basepath={''}>
+	<Router {url}>
 		<Nav />
-		<div>
+		<div class="h-[calc(100vh-80px)]">
 			<Route path=""><Home /></Route>
 			<Route path="info"><h1>Oh</h1></Route>
-			<Route path="commands"><h1>Oh</h1></Route>
+			<Route path="commands/:name" let:params
+				><Command command={params.name} /></Route
+			>
+			<Route path="commands"><CommandForward /></Route>
 			<Route path="stats"><h1>Oh</h1></Route>
 			<Route path="callback"><Callback /></Route>
 		</div>
@@ -23,16 +28,16 @@
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-      'Open Sans', 'Helvetica Neue', sans-serif;
-    background-color: #282828;
-    color: #ffffff;
-    height: 100%;
-  }
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+			Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		background-color: #282828;
+		color: #ffffff;
+		height: 100%;
+	}
 
-  *::after,
-  *::before {
-    box-sizing: border-box;
-  }
+	*::after,
+	*::before {
+		box-sizing: border-box;
+	}
 </style>
