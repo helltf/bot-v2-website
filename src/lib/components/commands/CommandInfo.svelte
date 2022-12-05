81264d860b409a5434544e0ca11682f84876f775
helltf <dario.zachow5530@gmail.com>
2022-12-05 13:35:40 +0100

    chore: use card for command info

--- src/lib/components/commands/CommandInfo.svelte
+++ src/lib/components/commands/CommandInfo.svelte
@@ -3,5 +3,7 @@
 	export let info: string
 </script>
 
-<h3>{heading}</h3>
-<p>{info}</p>
+<div class='h-28 border-gray-600 border-2 rounded-xl p-2'>
+<h2 class="text-xs">{heading}</h2>
+  <p class="text-2xl">{info}</p>
+</div>
