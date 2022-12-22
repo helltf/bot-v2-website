4a6b75f1d8e4e8ecbc44353a421368eb775c1e14
helltf <dario.zachow5530@gmail.com>
2022-12-22 12:21:08 +0100

    feat: correctly parse command info

--- src/lib/components/commands/SelectedCommand.svelte
+++ src/lib/components/commands/SelectedCommand.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
 	import { getCommandInfo, type Command } from '../../js/commands'
 	import CommandInfo from './CommandInfo.svelte'
 
@@ -12,13 +11,20 @@
 		cooldown: 'Cooldown',
 		counter: 'Count',
 		deleted: 'Disabled',
-		optionalParams: 'Optional Parameters',
-		requiredParams: 'Required Parameters',
+		optional_params: 'Optional Parameters',
+		required_params: 'Required Parameters',
 		permissions: 'Minimal Permissions'
 	}
 
   const onChange = async(selectedCommand: string) => {
     command = await getCommandInfo(selectedCommand)
+  }
+
+  const parse = (data: string | string[] | number):string => {
+    if(typeof data === 'string' ) return data as string
+    if(typeof data === 'object') {
+      return data.length > 0 ? data.join(", "): 'none' }
+    return `${data}`
   }
 
   $: onChange(selected)
@@ -29,7 +35,7 @@
 		<h2 class="text-5xl mb-10">{command.name}</h2>
     <div class="grid grid-cols-2 gap-6">
 		{#each Object.entries(mappings) as [key, value]}
-			<CommandInfo heading={value} info={command[key] ?? 'none'} />
+			<CommandInfo heading={value} info={parse(command[key])} />
 		{/each}
     </div>
 	</div>
